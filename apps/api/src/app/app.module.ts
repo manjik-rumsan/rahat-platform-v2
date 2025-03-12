fa24cba60314e1de1be4eb886492fa9f7af89dd9
nabina-rumsan <nabina@rumsan.net>
2025-03-12 16:16:03 +0545

    user module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,23 +1,31 @@
-import {Module} from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
-import {EventEmitterModule} from '@nestjs/event-emitter';
-import {DemoModule} from 'src/demo/demo.module';
-import {ListenerModule} from '../listeners/listener.module';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {WebSocketService} from './websocket.service';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import {
+  AuthsModule,
+  RSUserModule,
+  RolesModule,
+  UsersModule,
+} from '@rumsan/user';
+import { DemoModule } from 'src/demo/demo.module';
+import { ListenerModule } from '../listeners/listener.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { WebSocketService } from './websocket.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true }),
     EventEmitterModule.forRoot({
       maxListeners: 10,
       ignoreErrors: false,
     }),
     DemoModule,
     ListenerModule,
+    RSUserModule.forRoot([AuthsModule, UsersModule, RolesModule]),
+
   ],
   controllers: [AppController],
   providers: [AppService, WebSocketService],
 })
-export class AppModule {}
+export class AppModule { }
--- packages/sdk/src/constants/events.ts
+++ packages/sdk/src/constants/events.ts
@@ -4,3 +4,30 @@ export const EVENTS = {
     PONG: 'pong',
   },
 };
+
+export const UserRoles = {
+  ADMIN: 'Admin',
+  USER: 'User',
+  VENDOR: 'Vendor',
+};
+
+export const ACTIONS = {
+  MANAGE: 'manage',
+  CREATE: 'create',
+  UPDATE: 'update',
+  DELETE: 'delete',
+  READ: 'read',
+};
+
+export const APP = {
+  JWT_BEARER: 'JWT',
+};
+
+export const SUBJECTS = {
+  ALL: 'all',
+  BENEFICIARY: 'beneficiary',
+  PROJECT: 'project',
+  VENDOR: 'vendor',
+  PUBLIC: 'public',
+};
+
