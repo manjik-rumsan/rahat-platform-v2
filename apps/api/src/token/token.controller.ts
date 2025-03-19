b9b20f0bb0020d2c1b7eb3c1af55a3ccf7942231
nabina-rumsan <nabina@rumsan.net>
2025-03-19 15:11:15 +0545

    token module

---
+++ apps/api/src/token/token.controller.ts
@@ -0,0 +1,37 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateTokenDto } from './dto/create-token.dto';
+import { TokenService } from './token.service';
+
+@Controller('token')
+@ApiTags('Tokens')
+export class TokenController {
+  constructor(private readonly tokenService: TokenService) { }
+
+  @Post()
+  create(@Body() createTokenDto: CreateTokenDto) {
+    return this.tokenService.create(createTokenDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.tokenService.findAll();
+  }
+
+  @Get(':contractAddress')
+  findOne(@Param('contractAddress') contractAddress: string) {
+    return this.tokenService.findOne(contractAddress);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateTokenDto: UpdateTokenDto) {
+  //   return this.tokenService.update(+id, updateTokenDto);
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tokenService.remove(+id);
+  }
+}
